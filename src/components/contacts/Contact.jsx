550a9f1fd5818fe48d8c4f726137bc45363074fd
Lona Sontsele <1867792@students.wits.ac.za>
2022-08-05 18:00:44 +0200

    First commit

---
+++ src/components/contacts/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './contact.css';
+
+const Contact = () => {
+  return (
+    <div>
+      Cont
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './nav.css';
+
+const Nav = () => {
+  return (
+    <div>
+      navigate
+    </div>
+  )
+}
+
+export default Nav
